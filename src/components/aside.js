319b6c2dc73734a482592f97267af767e92ec558
Michael Bergen <michael.bergen@perficient.com>
2019-08-15 08:46:51 -0700

    memoize aside to prevent unnecessary rendering

--- src/components/aside.js
+++ src/components/aside.js
@@ -1,9 +1,5 @@
 import React from 'react';
 
-// const equalAside = (prevProps, nextProps) => {
-//   return prevProps.list.length === nextProps.list.length;
-// }
-
 const Aside = ({list}) => (
   <aside className="aside">
     <ul>
@@ -11,12 +7,9 @@ const Aside = ({list}) => (
         <li key={i}><a href={item.link}>{item.title}</a></li>
       ))}
     </ul>
-    <div style={{paddingLeft: '10px', maxWidth: '120px'}}>
-      <a href="/" style={{marginRight: '5px'}}>Apartment #01F - 0204</a><span className="fa fa-heart"></span>
-    </div>
   </aside>
 );
 
-const MemoizedAside = Aside;
+const MemoizedAside = React.memo(Aside);
 
 export default MemoizedAside;
